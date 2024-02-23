51e8a762b6657a0ce12f198ab46ba2cada2d9aa0
Himanshu Pareek <himanshupareekiit01@gmail.com>
2024-02-23 21:36:37 +0530

    Created Public OAuth2 Client.

---
+++ spa-client/assets/js/constants.js
@@ -0,0 +1,10 @@
+// Related to Authorization Server
+export const AUTHORIZE_ENDPOINT = 'http://localhost:8080/realms/OAuth2%20Demo/protocol/openid-connect/auth';
+export const TOKEN_ENDPOINT = 'http://localhost:8080/realms/OAuth2%20Demo/protocol/openid-connect/token';
+
+// Related to Resource Server
+export const API_ENDPOINT = 'http://localhost:8080/realms/OAuth2%20Demo/protocol/openid-connect/userinfo';
+
+// Related to Client Application
+export const CLIENT_ID = 'public-client';
+export const REDIRECT_URI_SUFFIX = '/oauth/callback/';
