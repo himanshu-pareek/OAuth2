bd1b080b69719f84a827224676b5d8c9f860cee1
Himanshu Pareek <himanshu@indihood.com>
2024-08-13 10:27:35 +0530

    Removes unnecessary test.

--- authorization-server/src/test/java/dev/javarush/oauth2/authorizationserver/crypto/JWTSignerVerifierTest.java
+++ authorization-server/src/test/java/dev/javarush/oauth2/authorizationserver/crypto/JWTSignerVerifierTest.java
@@ -51,21 +51,8 @@ class JWTSignerVerifierTest {
 
     @Test
     public void test2() throws Exception {
-        var mapJWTVerifier = getMapJWTVerifier();
-        String kid = (String) mapJWTVerifier.getHeader().get("kid");
-        JWKSet jwkSet = getJWKSetFromUri("https://www.googleapis.com/service_accounts/v1/jwk/securetoken@system.gserviceaccount.com");
-        JWK jwk = jwkSet.keys().stream()
-            .filter(jw -> jw.kid().equals(kid))
-            .findFirst()
-            .orElseThrow(() -> new RuntimeException("JWK not found for " + kid));
-        PublicKey publicKey = createPublicKeyFromJWK(jwk);
-        Map token = mapJWTVerifier.verify(publicKey).getToken();
-        System.out.println(token);
-    }
-
-    private static JWTVerifier<Map> getMapJWTVerifier() {
-        String jwt = "eyJhbGciOiJSUzI1NiIsImtpZCI6IjFkYmUwNmI1ZDdjMmE3YzA0NDU2MzA2MWZmMGZlYTM3NzQwYjg2YmMiLCJ0eXAiOiJKV1QifQ.eyJpc3MiOiJodHRwczovL3NlY3VyZXRva2VuLmdvb2dsZS5jb20vaW5kaWhvb2QtZGV2LWluIiwiYXVkIjoiaW5kaWhvb2QtZGV2LWluIiwiYXV0aF90aW1lIjoxNzIyNjAwODc5LCJ1c2VyX2lkIjoicUhUWTlkUHFCQ1ZqemJPeDJTaUhYQzZmQWt5MSIsInN1YiI6InFIVFk5ZFBxQkNWanpiT3gyU2lIWEM2ZkFreTEiLCJpYXQiOjE3MjI2MDA4NzksImV4cCI6MTcyMjYwNDQ3OSwiZW1haWwiOiJ0ZXN0MWluZGlAeWFob28uY29tIiwiZW1haWxfdmVyaWZpZWQiOnRydWUsImZpcmViYXNlIjp7ImlkZW50aXRpZXMiOnsiZW1haWwiOlsidGVzdDFpbmRpQHlhaG9vLmNvbSJdfSwic2lnbl9pbl9wcm92aWRlciI6InBhc3N3b3JkIn19.GhCoXrrprjctIGRbrHeWSnB-MwNz778D3cyj5dMY0MoUAO8HuXTJ692BuX2Trk2i3j6BfxaOO7SuAuCpuKfq8J5lo2NEXSxCZqoJQXUU4aTU9Y1bKO6iPKlLa0cuFU8uqspvq_E57rSU74izkMLFEeoApfz1dTIQrFTQL5WZy0VCeQk5G1lMZvLlBsKjUramwKdV9LGXcM72I3GTZe9MVtMpaM523_JoOu_mSll6Ygu0G_at6whgz2e72du9wVUDIjg7jno2T9KkJKf0ZXLwr-KAH7QAnTqVBnyPNAF-ivSPI-ovVu5FporBzFZaYaoguKOVtyCBGc9VgnyaxjfbVg";
-      return new JWTVerifier<>(jwt, Map.class);
+      JWKSet jwkSet = getJWKSetFromUri("https://www.googleapis.com/service_accounts/v1/jwk/securetoken@system.gserviceaccount.com");
+      System.out.println(jwkSet);
     }
 
     private PublicKey createPublicKeyFromJWK(JWK jwk) {
